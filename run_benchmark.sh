7eb67c17f18889046e5b88529ab40c7049340b2e
timotejblazic <timi.blazic@gmail.com>
2026-05-26 11:59:50 +0200

    generate gif

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --reservation=fri
 #SBATCH --job-name=lenia_mpi
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=32
 #SBATCH --nodes=1
 #SBATCH --output=slurm_temp_%j.log
 #SBATCH --hint=nomultithread
@@ -19,10 +19,10 @@ make
 # NASTAVITVE MERITEV
 # ==========================================
 
-PROGRAM_NAME="1_row_wise" # Ime za datoteke in podmapo
+PROGRAM_NAME="2_new_mpi_gif" # Ime za datoteke in podmapo
 
-RUNS=5                  # Število ponovitev za povprečje
-N=128                   # TUKAJ ROČNO SPREMENIŠ VELIKOST (128, 512, 1024, 2048, 4096)
+RUNS=1                  # Število ponovitev za povprečje
+N=1024                   # TUKAJ ROČNO SPREMENIŠ VELIKOST (128, 512, 1024, 2048, 4096)
 STEPS=100               # Število korakov simulacije
 
 # Število MPI procesov pride iz Slurma.
